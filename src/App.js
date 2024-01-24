640a72c59ad6aa53fc1ccfda2858887ac0dd95ae
Igor Rusovitch <igy1986@gmail.com>
2024-01-24 21:07:29 +0200

    ui react-bootstrap & activePrice

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import './App.scss'
+import { useState } from "react";
+import "./App.scss";
+import Container from "react-bootstrap/Container";
+import Body from "./Body";
+import Head, {DEFAULT_ACTIVE_BUTTON} from "./Head";
+import Footer from "./Footer";
 
 function App() {
+  const [activePrice, setActivePrice] = useState(DEFAULT_ACTIVE_BUTTON);
+
   return (
-    <></>
+    <Container>
+      <Head activePrice={activePrice} setActivePrice={setActivePrice} />
+      <Body />
+      <Footer />
+    </Container>
   );
 }
 
---
+++ src/Body/Body.jsx
@@ -0,0 +1,20 @@
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+const Body = () => {
+  return (
+    <>
+      <Row>
+        <Col></Col>
+      </Row>
+      <Row>
+        <Col></Col>
+      </Row>
+      <Row>
+        <Col></Col>
+      </Row>
+    </>
+  );
+};
+
+export default Body;
