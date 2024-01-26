4005edccee2f47f31ca3575669c20526ce757508
Igor Rusovitch <igy1986@gmail.com>
2024-01-26 22:28:29 +0200

    implement consumption duration buttons

--- src/App.js
+++ src/App.js
@@ -2,17 +2,22 @@ import { useState } from "react";
 import "./App.scss";
 import Container from "react-bootstrap/Container";
 import Body from "./Body";
-import Head, {DEFAULT_ACTIVE_PRICE_BUTTON_ID} from "./Head";
+import Head, { DEFAULT_ACTIVE_PRICE_BUTTON_ID } from "./Head";
 import Footer from "./Footer";
 
 function App() {
   const [activePrice, setActivePrice] = useState(DEFAULT_ACTIVE_PRICE_BUTTON_ID);
+  const [activeHour, setActiveHour] = useState();
 
   return (
     <Container>
       <Head activePrice={activePrice} setActivePrice={setActivePrice} />
-      <Body/>
-      <Footer activePrice={activePrice}/>
+      <Body activeHour={activeHour} />
+      <Footer
+        activePrice={activePrice}
+        activeHour={activeHour}
+        setActiveHour={setActiveHour}
+      />
     </Container>
   );
 }
--- src/Footer/Intervals.jsx
+++ src/Footer/Intervals.jsx
@@ -1,12 +1,34 @@
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import { Button } from "react-bootstrap";
+import Stack from "react-bootstrap/Stack";
+import { INTERVAL_OPTIONS } from "./configuration";
 
-const Intervals = ({ intervalOptions }) => {
+const Intervals = ({ activeHour, setActiveHour }) => {
   return (
     <Row>
-      {intervalOptions.durations.map((duration, index) => (
-        <Col key={index}>{duration} {intervalOptions.timeUnit}</Col>
-      ))}
+      <Col>
+        <Stack
+          direction="horizontal"
+          gap={3}
+          className="justify-content-center"
+        >
+          {INTERVAL_OPTIONS.map(({ id, name }) => {
+            return (
+              <Button
+                key={id}
+                onClick={() => {
+                  setActiveHour(id);
+                }}
+                variant="outline-warning"
+                active={(activeHour || 1) === id}
+              >
+                {name}
+              </Button>
+            );
+          })}
+        </Stack>
+      </Col>
     </Row>
   );
 };
