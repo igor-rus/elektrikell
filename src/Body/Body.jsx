ecc9d7b7190834572f8d143d04a44adf21fadda8
Igor Rusovitch <igy1986@gmail.com>
2024-01-26 22:25:53 +0200

    import recharts, integrate chart into body

--- src/Body/Body.jsx
+++ src/Body/Body.jsx
@@ -1,18 +1,34 @@
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import { CHART_DATA } from "./constants";
+import {
+  LineChart,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+  Line,
+  ResponsiveContainer,
+} from "recharts";
 
 const Body = () => {
   return (
-    //temporary inline style just for visual separation, will remove later.
     <>
       <Row>
-        <Col></Col>
-      </Row>
-      <Row>
-        <Col></Col>
-      </Row>
-      <Row>
-        <Col></Col>
+        <Col>
+          <ResponsiveContainer width="100%" height={400}>
+            <LineChart data={CHART_DATA}>
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="name" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Line type="monotone" dataKey="pv" stroke="#8884d8" />
+              <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+            </LineChart>
+          </ResponsiveContainer>
+        </Col>
       </Row>
     </>
   );
