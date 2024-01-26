af7db93d6de3169dab6054152138a3cdb5dea633
Igor Rusovits <igy1986@gmail.com>
2024-01-26 17:57:02 +0200

    Homework 1 (#3)
    * add temp styling for body
    
    * conditional footer rendering, better naming for default price button constant
    
    * make placeholder for countdown timer
    
    * add unstyled content for targertHigh and targetLow components
    
    * remove console log
    
    * use constant instead of raw comparison

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -1,8 +1,9 @@
 import TargetHigh from "./TargetHigh";
 import TargetLow from "./TargetLow";
+import { DEFAULT_ACTIVE_PRICE_BUTTON_ID } from "../Head";
 
 const Footer = ({ activePrice }) => {
-  return <>{activePrice === "low" ? <TargetLow /> : <TargetHigh />}</>;
+  return <>{activePrice === DEFAULT_ACTIVE_PRICE_BUTTON_ID ? <TargetLow /> : <TargetHigh />}</>;
 };
 
 export default Footer;
